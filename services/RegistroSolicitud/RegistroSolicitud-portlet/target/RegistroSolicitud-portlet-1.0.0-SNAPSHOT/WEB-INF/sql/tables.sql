c5b02fe0216485f639fb2c29344ea6cc7bb4ba13
Álvaro Saugar <alvaro.saugar@gmail.com>
2019-08-21 03:15:59 +0200

    Se eliminan elemientos autogenerados

--- services/RegistroSolicitud/RegistroSolicitud-portlet/target/RegistroSolicitud-portlet-1.0.0-SNAPSHOT/WEB-INF/sql/tables.sql
+++
@@ -1,8 +0,0 @@
-create table RegistroSolicitud_Registro (
-	registroId LONG not null primary key,
-	nombre VARCHAR(75) null,
-	apellido VARCHAR(75) null,
-	email VARCHAR(75) null,
-	nacimiento DATE null,
-	alta DATE null
-);
